426e548ef76a3736781200f5e532de5ddf9ce6a9
taiki-fw <taiki_52@outlook.jp>
2019-10-23 12:10:34 +0900

    エイリアスを調節

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,6 +1,3 @@
  ln -sf ~/dotfiles/.vimrc ~/.vimrc
- ln -sf ~/dotfiles/colors ~/.vim
  
  ln -sf ~/dotfiles/.zshrc ~/.zshrc
- ln -sf ~/dotfiles/.zshenv ~/.zshenv
- ln -sf ~/dotfiles/.zprofile ~/.zprofile
