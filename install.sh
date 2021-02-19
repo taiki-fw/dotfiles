06babedc8f7c06246cedd0fc1fad86fd53987337
taiki-fw <taiki_52@outlook.jp>
2021-02-19 19:51:18 +0900

    :recycle:シンボリックリンクを張る処理にコメントを追加

--- install.sh
+++ install.sh
@@ -4,9 +4,11 @@
 set -u
 
 git clone https://github.com/taiki-fw/dotfiles.git
+
 BASEDIR=$(dirname $0)
 cd $BASEDIR
 
+# dotfilesのシンボリックをrootに張る
 for f in .??*; do
   [ "$f" = ".git" ] && continue
 
