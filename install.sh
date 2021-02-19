a918f4c5e6b64b85c41c89e2d306660fdc1e8fee
taiki-fw <taiki_52@outlook.jp>
2021-02-19 22:31:09 +0900

    :sparkles:installとdeployの処理を分割

--- install.sh
+++ install.sh
@@ -3,7 +3,11 @@
 # 未定義な変数があるとき途中で終了する
 set -u
 
-git clone https://github.com/taiki-fw/dotfiles.git
+if [ ! -d "dotfiles" ];then
+  echo "fetch dotfiles from github"
+
+  git clone https://github.com/taiki-fw/dotfiles.git
+fi
 
 cd dotfiles
 # dotfilesのシンボリックをrootに張る
